26b0380616a8689bd67767e12e2e9a67336330db
tangenss <41493619+tangenss@users.noreply.github.com>
2018-10-05 23:45:15 +0400

    test

---
+++ untitled/src/Pidor.java
@@ -0,0 +1,26 @@
+public class Pidor {
+    String name;
+    Boolean HasBoyfriend;
+    int age;
+
+
+    public Pidor(String name, int age){
+        this.name=name;
+        this.age= age;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void checkPidor (Pidor pidor) {
+        if (this.HasBoyfriend){
+
+
+        }
+
+    }
+
+
+}
